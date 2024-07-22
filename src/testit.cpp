d0fd2c5becf8d28fe173f6526df6f7e1c5de90eb
mastamysta <benjamin-read@hotmail.co.uk>
2024-07-22 18:46:06 +0100

    Add slightly gross nested concept. Wish the get_wrapper_val func could just be defined in terms of IWrapper.

--- src/testit.cpp
+++ src/testit.cpp
@@ -1,6 +1,55 @@
 #include <iostream>
+#include <concepts>
+
+
+template <typename T>
+concept IInterface = requires (T v)
+{
+    { auto(T::interface_member) } -> std::same_as<int>;
+};
+
+struct some_interface
+{
+    static constexpr int interface_member = 5;
+};
+
+template <IInterface T>
+struct wrapper_one
+{
+    int use_interface()
+    {
+        return T::interface_member * 5;
+    }
+};
+
+template <IInterface T>
+struct wrapper_two
+{
+    int use_interface()
+    {
+        return T::interface_member - 1;
+    }
+};
+
+// Now lets create some concept to try and tie the wrappers together
+template <template <typename> typename T, typename I>
+concept IWrapper = requires (T<I> v)
+{
+    { auto(v.use_interface()) } -> std::same_as<int>;
+};
+
+template <template <typename> typename T, typename V>
+requires IWrapper<T, V>
+auto get_wrapper_val_yuck(T<V> w)
+{
+    return w.use_interface();
+}
 
 int main()
 {
-    std::cout << "Hello world.\n";
+    wrapper_one<some_interface> o;
+    wrapper_two<some_interface> t;
+
+    std::cout << get_wrapper_val_yuck(o) << "\n";
+    std::cout << get_wrapper_val_yuck(t) << "\n";
 }
